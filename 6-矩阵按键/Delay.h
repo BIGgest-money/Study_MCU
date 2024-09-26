c9fd38b815572434ed513780852489aee8ac171d
BIG money <wenzihangdq@163.com>
2024-09-26 22:26:35 +0800

    matrixkey's method 1

---
+++ 6-矩阵按键/Delay.h
@@ -0,0 +1,8 @@
+#ifndef _DELAY_	H_
+#define _DELAY_	H_
+
+
+void Delay(unsigned int xms);//延迟函数声明
+
+#endif
+
---
+++ 6-矩阵按键/MatrixKey.h
@@ -0,0 +1,8 @@
+#ifndef  __MatrixKey_H__
+#define  __MatrixKey_H__
+
+
+unsigned char matrix_key(void);//矩阵按键
+
+#endif
+
