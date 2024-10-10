a0a505f287ae806344eaae7e3d926089bd936cdd
BIG money <wenzihangdq@163.com>
2024-10-10 23:21:00 +0800

    74hc595

---
+++ 74hc595/delay.h
@@ -0,0 +1,11 @@
+
+#ifndef  __DELAY_H__
+#define  __DELAY_H__
+#include "delay_ms.h"
+
+//typedef unsigned int u16;
+//typedef unsigned char u8;
+
+void Delay_25us(u16 xus);//声明延时函数
+
+#endif
---
+++ 74hc595/delay_ms.h
@@ -0,0 +1,11 @@
+#ifndef  __DELAY_MS_H__
+#define  __DELAY_MS_H__
+
+
+typedef unsigned int u16;
+typedef unsigned char u8;
+
+
+void delay_ms(u16 ms);//延时函数 以毫秒为单位
+
+#endif
