2fbf964fc5ec1a872046aca145a651cc578ab243
BIG money <wenzihangdq@163.com>
2024-10-18 15:40:27 +0800

    74hc595芯片控制led点阵

--- 74hc595/main.c
+++ 74hc595/main.c
@@ -1,5 +1,5 @@
 #include "regx52.h"
-#include "delay_ms.h"
+#include "delay.h"
 #include "hc595.h"
 #define LEDDZ_COL_PORT P0 //对点阵列的端口P0宏定义
 u8 hc595_buf[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};//A8-A1 低到高
@@ -13,7 +13,7 @@ void main()
 		{
 			hc595_write_data(0x00);//动态显示 消影
 			hc595_write_data(hc595_buf[i]);
-			delay_ms(500);//延时0.5s 
+			Delay_25us(50000);//延时0.5s 
 			
 			
 		}
